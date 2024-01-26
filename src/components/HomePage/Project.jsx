4da516cd56cf88c685eb760c6e9894761db9f7d5
ChiBaoo <chibaoluu@gmail.com>
2024-01-26 08:31:21 +0700

    homepage layout

--- src/components/HomePage/Project.jsx
+++ src/components/HomePage/Project.jsx
@@ -1,7 +1,171 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Project() {
   return (
-    <div>Project</div>
-  )
+    <div>
+      <div className="flex flex-wrap mx-40">
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+        <div className="bg-white w-1/4 pl-11 pt-5 mb-5 h-80 hover:bg-slate-100 rounded-md">
+          <Link to="">
+            <div className="w-full h-53">
+              <img
+                className="w-full h-full"
+                src={
+                  "https://tse1.mm.bing.net/th?id=OIP.4XB8NF1awQyApnQDDmBmQwHaEo&pid=Api&P=0&h=180"
+                }
+                alt="Description of your image"
+                maxWidth
+                maxHeight
+              />
+            </div>
+            <div className="mt-3 ml-5">
+              <h6 className="font-bold text-lg">Project Name</h6>
+              <p className="text-red-400 text-lg">30.000.000vnd -30m2</p>
+              <p className="text-blue-400 text-lg">Thu Duc, Ho Chi Minh</p>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,55 +1,14 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import storageService from "../config/storageService";
+import News from "../components/HomePage/News";
+import Project from "../components/HomePage/Project";
 
 function HomePage() {
   return (
-    <div>
-      
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
-      <div>ddasdsa</div>
+    <div className="mt-30 ">
+      <News />
+      <Project />
     </div>
   );
 }
